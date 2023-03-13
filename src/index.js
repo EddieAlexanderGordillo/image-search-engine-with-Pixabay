ddebedb381a3652515a2ed1fdfa65244a2763be3
Felipe Andrés López <alexgordillo.g@gmail.com>
2023-03-13 16:20:51 -0500

    feth with async and await

--- src/index.js
+++ src/index.js
@@ -36,17 +36,25 @@ function mostrarAlerta(mensaje) {
   }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
   const termino = document.querySelector('#termino').value;
   const key = '34242519-8750e7dc97091d192b31e81bc';
   const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}&page=${paginaActual}`;
-  fetch(url)
-    .then((respuesta) => respuesta.json())
-    .then((resultado) => {
-      console.log(resultado.hits);
-      totalPaginas = calcularPaginas(resultado.totalHits);
-      mostrarImagenes(resultado.hits);
-    });
+  // fetch(url)
+  //   .then((respuesta) => respuesta.json())
+  //   .then((resultado) => {
+  //     console.log(resultado.hits);
+  //     totalPaginas = calcularPaginas(resultado.totalHits);
+  //     mostrarImagenes(resultado.hits);
+  //   });
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    totalPaginas = calcularPaginas(resultado.totalHits);
+    mostrarImagenes(resultado.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
 // generador que va registrar la cantidad de elementos de acuerdo a las paginas
 function* crearPaginador(total) {
